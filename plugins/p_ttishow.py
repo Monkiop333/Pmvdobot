519dbad4a8b9575d89eaffc41e7018973ef7e892
Kichuprobotzzz <111451450+Kichuprobotzzz@users.noreply.github.com>
2024-05-27 15:34:44 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -175,7 +175,6 @@ async def get_ststs(bot, message):
     size = get_size(size)
     free = get_size(free)
     now = datetime.now()
-    delta = now - start_time
     uptime = get_readable_time(delta.seconds)
     ram = psutil.virtual_memory().percent
     cpu = psutil.cpu_percent()
