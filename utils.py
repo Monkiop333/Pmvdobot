33af161af4e14646b2d0b1c5c1d4014695cba506
Monkiop333 <Haramiopok@outlook.com>
2025-03-18 07:42:59 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -491,4 +491,4 @@ async def send_all(bot, userid, files, ident):
             file_id=file.file_id,
             caption=f_caption,
             protect_content=True if ident == "filep" else False,
-            reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ BOT UPDATES ⚔️', url="https://t.me/ckflicks") ] ] ))
+            reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ BOT UPDATES ⚔️', url="https://t.me/am_films") ] ] ))
